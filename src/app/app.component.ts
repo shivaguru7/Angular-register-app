819442f520c5a1c66a4e9a138299ec628fce90f0
SIVA KUMAR S <47846332+shivaguru7@users.noreply.github.com>
2023-06-23 11:09:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  registerForm!:FormGroup
+  submitted=false;
   title = 'RegisterValidation';
+  constructor(private formBuilder:FormBuilder){}
+  ngOnInit()
+  {
+    this.registerForm=this.formBuilder.group({
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(4),Validators.maxLength(14)]]
+    })
+  }
+  onSubmit()
+  {
+    this.submitted=true
+    if(this.registerForm.invalid)
+    {
+      return
+    }
+  }
 }
